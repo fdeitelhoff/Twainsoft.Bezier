6f0fe096b7111b3fa63837d235265d98f54bcf21
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2014-11-20 10:58:08 +0100

    Removed unnecessary files. Added FAKE as build tool.

---
+++ build.fsx
@@ -0,0 +1,24 @@
+﻿#r @"packages\FAKE\tools\FakeLib.dll"
+
+open Fake
+
+RestorePackages()
+
+let outputDir = "./output"
+let buildDir = outputDir + "/build"
+let version = "0.4"
+
+Target "Clean" (fun _ -> 
+    CleanDirs [buildDir]
+)
+
+Target "BuildLibrary" (fun _ ->
+    !! "./src/**/*.csproj"
+    |> MSBuildRelease buildDir "Build"
+    |> Log "Building app: "
+)
+
+"Clean"
+    ==> "BuildLibrary"
+
+RunTargetOrDefault "BuildLibrary"
